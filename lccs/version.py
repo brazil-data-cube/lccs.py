dc2da6b898d4dde98f44f3ae916cc4ac8da58ba5
gqueiroz <gribeiro.mg@gmail.com>
2019-12-14 13:56:16 -0300

    Adding package structure

---
+++ lccs/version.py
@@ -0,0 +1,16 @@
+#
+# This file is part of Python Client Library for the LCCS Web Service.
+# Copyright (C) 2019 INPE.
+#
+# Python Client Library for the LCCS Web Service is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+
+"""Version information for Python Client Library for the LCCS Web Service.
+
+This file is imported by ``lccs.__init__``,
+and parsed by ``setup.py``.
+"""
+
+
+__version__ = '0.2.0'
