9d99533bd47ee3e24482f070d38d64c865746001
Fabiana Zioti <fabiana_zioti@hotmail.com>
2025-08-08 15:59:22 -0300

    :zap: removing comments line

--- lccs/lccs.py
+++ lccs/lccs.py
@@ -28,35 +28,6 @@ from .style_formats import StyleFormats
 from .style_utils import SldGenerator
 from .utils import Utils
 
-#
-# This file is part of Python Client Library for the LCCS-WS.
-# Copyright (C) 2022 INPE.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/gpl-3.0.html>.
-#
-"""Python API client wrapper for LCCS-WS."""
-import json
-from typing import List
-
-from cachetools import LRUCache, cached
-
-from .classification_system import ClassificationSystem
-from .mappings import Mapping, MappingGroup
-from .style_formats import StyleFormats
-from .style_utils import SldGenerator
-from .utils import Utils
-
 
 class LCCS:
     """This class implements a Python API client wrapper for LCCS-WS.
